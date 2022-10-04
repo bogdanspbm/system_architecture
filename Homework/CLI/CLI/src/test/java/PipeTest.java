9e82e87f0f9e4231f5152599e607d0866f635d5b
Misha Diagilev <mishadyagilev@gmail.com>
2022-10-04 17:21:47 +0300

    tests Pipes

--- Homework/CLI/CLI/src/test/java/PipeTest.java
+++ Homework/CLI/CLI/src/test/java/PipeTest.java
@@ -42,4 +42,41 @@ public class PipeTest extends Assert {
         assert cmdWC.buildOutput().equals("2 8 0 ");
 
     }
+
+    @Test
+    public void simpleTestWithPipeAndExit() {
+        CommandFactory factory = new CommandFactory();
+
+        String[] words = Parser.parse("cat example.txt | exit");
+        getStack().putArray(words);
+
+        Command cmdCat = factory.createCommand(getStack().get());
+        cmdCat.execute();
+
+        Command cmdPipe = factory.createCommand(getStack().get());
+        cmdPipe.execute();
+
+        Command cmdExit = factory.createCommand(getStack().get());
+        assert cmdExit.buildOutput().equals("0");
+
+    }
+
+    @Test
+    public void simpleTestWithTwoPipesAndExit() {
+        CommandFactory factory = new CommandFactory();
+
+        String[] words = Parser.parse("cat example.txt | exit | cat example.txt");
+        getStack().putArray(words);
+
+        Command cmdCat = factory.createCommand(getStack().get());
+        cmdCat.execute();
+
+        Command cmdPipe = factory.createCommand(getStack().get());
+        cmdPipe.execute();
+
+        Command cmdExit = factory.createCommand(getStack().get());
+        assert cmdExit.buildOutput().equals("0");
+
+    }
+
 }
