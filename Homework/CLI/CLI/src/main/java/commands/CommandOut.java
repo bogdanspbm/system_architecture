5ef073242113a170fa06d92d375d35704258e1b9
yaroslav2k <iaroslav2k@gmail.com>
2022-10-15 18:45:15 +0300

    Cleanup

--- Homework/CLI/CLI/src/main/java/commands/CommandOut.java
+++ Homework/CLI/CLI/src/main/java/commands/CommandOut.java
@@ -29,7 +29,6 @@ public class CommandOut extends Command {
 
         try {
             StringBuilder result = new StringBuilder();
-            System.out.println(command.toString());
             Process p = Runtime.getRuntime().exec(command.toString());
             BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
             for (String line : in.lines().toList()) {
