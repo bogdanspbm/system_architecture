24f6e5569cc3ff849316347f87a134e2cdf5f0b7
Nickkreker <nick_berezikov@me.com>
2022-10-17 16:55:31 +0300

    Add missing tests

--- Homework/CLI/CLI/src/test/java/LSTest.java
+++ Homework/CLI/CLI/src/test/java/LSTest.java
@@ -22,24 +22,19 @@ import static utils.Stack.getStack;
 import static utils.Parser.parse;
 
 public class LSTest extends Assert {
-    
-    File fileA, fileB, fileC, fileD;
-    File subfolder;
-    File fileX, fileY, fileZ;
-
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
 
     @Before
     public void setUp() throws IOException {
-        fileA = folder.newFile("fileA.txt");
-        fileB = folder.newFile("img.png");
-        fileC = folder.newFile("main.py");
-        fileD = folder.newFile("source.java");
-        subfolder = folder.newFolder("subfolder");
-        fileX = folder.newFile("subfolder/magical-tree.jpg");
-        fileY = folder.newFile("subfolder/song.wav");
-        fileZ = folder.newFile("subfolder/map.pdf");
+        folder.newFile("fileA.txt");
+        folder.newFile("img.png");
+        folder.newFile("main.py");
+        folder.newFile("source.java");
+        folder.newFolder("subfolder");
+        folder.newFile("subfolder/magical-tree.jpg");
+        folder.newFile("subfolder/song.wav");
+        folder.newFile("subfolder/map.pdf");
     }
 
     @Test
@@ -86,6 +81,91 @@ public class LSTest extends Assert {
         Command commandLS = factory.createCommand(getStack().get());
         
         String expectedOutput = "fileA.txt img.png main.py source.java subfolder \n";
+        String actualOutput = commandLS.buildOutput();
+        assert equalOutputs(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testOnLsWithOneFileParam() {
+        String filePath = Paths.get(System.getProperty("java.io.tmpdir"),
+                                       folder.getRoot().getName(), "img.png").toString();
+
+        CommandFactory factory = new CommandFactory();
+
+        String[] words = Parser.parse(String.format("ls %s", filePath));
+        getStack().putArray(words);
+
+        Command commandLS = factory.createCommand(getStack().get());
+        
+        String expectedOutput = String.format("%s \n", filePath);
+        String actualOutput = commandLS.buildOutput();
+        assert equalOutputs(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testOnLsWithMultipleFiles() {
+        String tempDirPath = Paths.get(System.getProperty("java.io.tmpdir"),
+                                       folder.getRoot().getName()).toString();
+
+        CommandFactory factory = new CommandFactory();
+
+        String fileAPath = Paths.get(tempDirPath, "source.java").toString();
+        String fileBPath = Paths.get(tempDirPath, "main.py").toString();
+        String[] words = Parser.parse(String.format("ls %s %s", fileAPath, fileBPath));
+        getStack().putArray(words);
+
+        Command commandLS = factory.createCommand(getStack().get());
+        
+        String expectedOutput = String.format("%s %s \n", fileAPath, fileBPath);
+        String actualOutput = commandLS.buildOutput();
+        assert equalOutputs(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testOnLsWithMultipleFolders() {
+        String tempDirPath = Paths.get(System.getProperty("java.io.tmpdir"),
+                                       folder.getRoot().getName()).toString();
+        
+        String tempSubdirPath = Paths.get(tempDirPath, "subfolder").toString();
+
+        CommandFactory factory = new CommandFactory();
+
+        String[] words = Parser.parse(String.format("ls %s %s", tempDirPath, tempSubdirPath));
+        getStack().putArray(words);
+
+        Command commandLS = factory.createCommand(getStack().get());
+        
+        String expectedOutput = String.format("%s:\n%s \n\n%s:\n%s \n", tempDirPath,
+                                              "fileA.txt img.png main.py source.java subfolder",
+                                              tempSubdirPath,
+                                              "magical-tree.jpg song.wav map.pdf");
+                                              
+        String actualOutput = commandLS.buildOutput();
+        assert equalOutputs(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testOnLsWithAllParamTypes() {
+        String tempDirPath = Paths.get(System.getProperty("java.io.tmpdir"),
+                                       folder.getRoot().getName()).toString();
+        
+        String fileAPath = Paths.get(tempDirPath, "fileA.txt").toString();
+        String fileBPath = Paths.get(tempDirPath, "main.py").toString();
+        String absentFilePath = Paths.get(tempDirPath, "absent").toString();
+
+        CommandFactory factory = new CommandFactory();
+
+        String[] words = Parser.parse(String.format("ls %s %s %s %s", fileAPath, tempDirPath, fileBPath, absentFilePath));
+        getStack().putArray(words);
+
+        Command commandLS = factory.createCommand(getStack().get());
+        
+        String expectedOutput = String.format("ls: cannot access '%s': No such file or directory\n%s %s \n\n%s:\n%s \n",
+                                              absentFilePath,
+                                              fileAPath, fileBPath,
+                                              tempDirPath,
+                                              "fileA.txt img.png main.py source.java subfolder");
+                                              
         String actualOutput = commandLS.buildOutput();
         assert equalOutputs(expectedOutput, actualOutput);
 
